f02b2b39acfbb41cda82b26715f05a36eb7db3c7
Mostafa Ahmed <mostafamohamedhamed@outlook.com>
2021-12-21 20:43:32 +0200

    -Removing unused files

--- EnergiaProducts.Persistence/Constance/CateogryIds.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EnergiaProducts.Persistence.Constance
-{
-    internal class CateogryIds
-    {
-        public const string BakeryCategoryId = "6ab9f515-0fbb-4888-9eb2-b92df3b580d3";
-        public const string OtherShitCategoryId = "17be80a9-bc92-4b0b-abae-cdb130289262";
-    }
-}
