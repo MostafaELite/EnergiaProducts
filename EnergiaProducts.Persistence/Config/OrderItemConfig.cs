d302cb52ed4ea018f274476b1432a4d374f4c1cc
Mostafa Ahmed <mostafamohamedhamed@outlook.com>
2021-12-25 03:07:24 +0200

    Revert "-Removing unused files"
    This reverts commit f02b2b39acfbb41cda82b26715f05a36eb7db3c7.

---
+++ EnergiaProducts.Persistence/Config/OrderItemConfig.cs
@@ -0,0 +1,14 @@
+﻿using EnergiaProducts.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnergiaProducts.Persistence.Config
+{
+    public class OrderItemConfig
+    {
+        public static void Configure(ModelBuilder builder)
+        {
+            var entity = builder.Entity<OrderItem>();
+            entity.HasKey(item => new { item.ProductId, item.OrderId });
+        }
+    }
+}
